ca78d93c240adeabd04c1a139f64b4be1ac63992
Suraj Khot <142373628+suraj-khot-19@users.noreply.github.com>
2024-12-23 07:02:38 +0530

    Create row-wise-sorted.java

---
+++ GFG/row-wise-sorted.java
@@ -0,0 +1,68 @@
+package GFG;
+/*
+Given a row-wise sorted 2D matrix mat[][] of size n x m and an integer x, find whether element x is present in the matrix.
+Note: In a row-wise sorted matrix, each row is sorted in itself, i.e. for any i, j within bounds, mat[i][j] <= mat[i][j+1].
+
+Examples :
+
+Input: mat[][] = [[3, 4, 9],[2, 5, 6],[9, 25, 27]], x = 9
+Output: true
+Explanation: 9 is present in the matrix, so the output is true.
+Input: mat[][] = [[19, 22, 27, 38, 55, 67]], x = 56
+Output: false
+Explanation: 56 is not present in the matrix, so the output is false.
+*/
+
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG05 {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t > 0) {
+            int rows = sc.nextInt();
+            int columns = sc.nextInt();
+
+            int matrix[][] = new int[rows][columns];
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            int target = sc.nextInt();
+
+            Solution x = new Solution();
+
+            if (x.searchRowMatrix(matrix, target))
+                System.out.println("true");
+            else
+                System.out.println("false");
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution05 {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchRowMatrix(int[][] mat, int x) {
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                if(mat[i][j]==x){
+                    return true;
+                }
+      
+      }
+        }
+        return false;
+    }
+}
