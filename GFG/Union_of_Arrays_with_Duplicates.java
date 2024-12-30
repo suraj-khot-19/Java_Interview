460b86223196617819c99b686afc2d58bc7132ed
Suraj Khot <142373628+suraj-khot-19@users.noreply.github.com>
2024-12-30 08:20:26 +0530

    Solved  Union_of_Arrays_with_Duplicates.java

---
+++ GFG/Union_of_Arrays_with_Duplicates.java
@@ -0,0 +1,85 @@
+package GFG;
+/*
+Given two arrays a[] and b[], the task is to find the number of elements in the union between these two arrays.
+
+The Union of the two arrays can be defined as the set containing distinct elements from both arrays. If there are repetitions, then only one element occurrence should be there in the union.
+
+Note: Elements of a[] and b[] are not necessarily distinct.
+
+Examples
+
+Input: a[] = [1, 2, 3, 4, 5], b[] = [1, 2, 3]
+Output: 5
+Explanation: Union set of both the arrays will be 1, 2, 3, 4 and 5. So count is 5.
+Input: a[] = [85, 25, 1, 32, 54, 6], b[] = [85, 2] 
+Output: 7
+Explanation: Union set of both the arrays will be 85, 25, 1, 32, 54, 6, and 2. So count is 7.
+Input: a[] = [1, 2, 1, 1, 2], b[] = [2, 2, 1, 2, 1] 
+Output: 2
+Explanation: We need to consider only distinct. So count of elements in union set will be 2.
+*/
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG11 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            // Reading the first array
+            String line1 = read.readLine().trim();
+            String[] numsStr1 = line1.split(" ");
+            int[] a = new int[numsStr1.length];
+            for (int i = 0; i < numsStr1.length; i++) {
+                a[i] = Integer.parseInt(numsStr1[i]);
+            }
+
+            // Reading the second array
+            String line2 = read.readLine().trim();
+            String[] numsStr2 = line2.split(" ");
+            int[] b = new int[numsStr2.length];
+            for (int i = 0; i < numsStr2.length; i++) {
+                b[i] = Integer.parseInt(numsStr2[i]);
+            }
+
+            // Creating an instance of the Solution class
+            Solution11 ob = new Solution11();
+
+            // Calling doUnion method and printing the result
+            System.out.println(ob.findUnion(a, b));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution11 {
+    public static int findUnion(int a[], int b[]) {
+        // Using a HashSet to store distinct elements
+        HashSet<Integer> unionSet = new HashSet<>();
+        
+        // Adding all elements of array a to the set
+        for (int num : a) {
+            unionSet.add(num);
+        }
+        
+        // Adding all elements of array b to the set
+        for (int num : b) {
+            unionSet.add(num);
+        }
+        
+        // Returning the size of the set as the count of the union
+        return unionSet.size();
+    }
+}
