756839675ef536475d8661d691272ee145b9a9f3
Suraj Khot <142373628+suraj-khot-19@users.noreply.github.com>
2025-01-01 04:14:17 +0530

    Updates

--- GFG/Find_All_Triplets_with_Zero_Sum.java
+++ GFG/Find_All_Triplets_with_Zero_Sum.java
@@ -1,6 +1,6 @@
 package GFG;
 /*
-Given an array arr[], find all possible triplets i, j, k in the arr[] whose sum of elements is equals to zero. 
+Given an array = arr[], find all possible triplets i, j, k in the arr[] whose sum of elements is equals to zero. 
 Returned triplet should also be internally sorted i.e. i<j<k.
 
 Examples:
