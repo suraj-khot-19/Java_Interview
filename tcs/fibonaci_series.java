4df9f9402188fe6cd06ed9061bf283fdc6034259
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-06 21:30:18 +0530

    tcs interview

--- tcs/fibonaci_series.java
+++ tcs/fibonaci_series.java
@@ -4,8 +4,9 @@ public class fibonaci_series {
     public static void main(String[] args) {
         int first=0;
         int secound=1;
+        System.out.println(first+"\n"+secound);
         int n=10;
-        for (int i=1;i<=n;i++){
+        for (int i=3;i<=n;i++){
             int next=first+secound;
             System.out.println(next+" ");
             first=secound;
