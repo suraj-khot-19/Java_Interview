fc0a0af16b654c4819dfcd77dc118c28d13bd801
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-11 21:24:45 +0530

    hashmap keys only

---
+++ hashmap/hashmap_only_keys_print.java
@@ -0,0 +1,30 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class hashmap_only_keys_print {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map=new HashMap<>();
+
+        //insert in hashmap
+        map.put("Zoho",90);
+        map.put("Suraj",82);
+        map.put("Sonya",80);
+
+        //creating set of only keys of hashmaps
+        Set<String> keys=map.keySet();
+
+        System.out.println(keys); // array set of a keys
+
+        for (String key : keys){
+            System.out.println(key);
+        }
+    }
+    /*
+    [Suraj, Zoho, Sonya]
+    Suraj
+    Zoho
+    Sonya
+     */
+}
